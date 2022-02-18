5d11483fab47aa43d9d77828b33948d4c0b51e9b
foxytanuki <biz@foxytanuki.dev>
2022-02-18 19:08:22 +0900

    Fix github username

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: 'foxytanuki',
-  github: 'foxytanki',
+  github: 'foxytanuki',
   linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
